0c36dab727ef25e7ca0cae52390a67f39e5f570f
Isha Pali <ishapali30.1997@gmail.com>
2025-06-06 17:20:47 +0530

    Update Surrogate_IDS.py

--- PLAID/Surrogate_IDS.py
+++ PLAID/Surrogate_IDS.py
@@ -20,140 +20,6 @@ data_transforms = {
     'train': transforms.Compose([transforms.ToTensor()])
 }
 
-class InceptionStem(nn.Module):
-    def __init__(self):
-        super(InceptionStem, self).__init__()
-        self.stem = nn.Sequential(
-            nn.Conv2d(in_channels = 3, out_channels = 32, stride = 1, kernel_size = 3, padding = 'same'),
-            nn.Conv2d(in_channels = 32, out_channels = 32, stride = 1, kernel_size = 3, padding = 'valid'),
-            nn.MaxPool2d(kernel_size = 3, stride = 2, padding = 0),
-            nn.Conv2d(in_channels = 32, out_channels = 64, kernel_size = 1, stride = 1, padding = 'valid'),
-            nn.Conv2d(in_channels = 64, out_channels = 128, kernel_size = 3, stride = 1, padding = 'same'),
-            nn.Conv2d(in_channels = 128, out_channels = 128, kernel_size = 3, stride = 1, padding = 'same')
-        )
-    
-    def forward(self, x):
-        stem_out = self.stem(x)
-        return stem_out
-    
-
-class InceptionResNetABlock(nn.Module):
-    def __init__(self, in_channels = 128, scale=0.17):
-        super(InceptionResNetABlock, self).__init__()
-        self.scale = scale
-        self.branch0 = nn.Conv2d(in_channels, 32, kernel_size=1, stride=1, padding='same')
-        self.branch1 = nn.Sequential(
-            nn.Conv2d(in_channels, 32, kernel_size=1, stride=1, padding='same'),
-            nn.Conv2d(32, 32, kernel_size=3, stride=1, padding='same')
-        )
-        self.branch2 = nn.Sequential(
-            nn.Conv2d(in_channels, 32, kernel_size=1, stride=1, padding='same'),
-            nn.Conv2d(32, 32, kernel_size=3, stride=1, padding='same'),
-            nn.Conv2d(32, 32, kernel_size=3, stride=1, padding='same')
-        )
-        self.conv_up = nn.Conv2d(96, 128, kernel_size=1, stride=1, padding='same')
-    
-    def forward(self, x):
-        branch0 = self.branch0(x)
-        branch1 = self.branch1(x)
-        branch2 = self.branch2(x)
-        mixed = torch.cat([branch0, branch1, branch2], dim=1)
-        up = self.conv_up(mixed)
-        return F.relu(x + self.scale * up)
-    
-
-class ReductionA(nn.Module):
-    def __init__(self, in_channels = 128):
-        super(ReductionA, self).__init__()
-        self.branch0 = nn.Conv2d(in_channels = in_channels, out_channels = 192, kernel_size = 3, stride = 2, padding = 'valid')
-        self.branch1 = nn.Sequential(
-            nn.Conv2d(in_channels = in_channels, out_channels = 96, kernel_size = 1, stride = 1, padding = 'same'),
-            nn.Conv2d(in_channels = 96, out_channels = 96, kernel_size = 3, stride = 1, padding = 'same'),
-            nn.Conv2d(in_channels = 96, out_channels = 128, kernel_size = 3, stride = 2, padding = 'valid')
-        )
-        self.branch2  = nn.MaxPool2d(kernel_size = 3, stride = 2, padding = 0)
-
-    def forward(self, x):
-        branch0 = self.branch0(x)
-        branch1 = self.branch1(x)
-        branch2 = self.branch2(x)
-        mixed = torch.cat([branch0, branch1, branch2], dim = 1)
-        return mixed
-    
-class InceptionResNetBBlock(nn.Module):
-    def __init__(self, in_channels = 448, scale = 0.10):
-        super(InceptionResNetBBlock, self).__init__()
-        self.scale = scale
-        self.branch0 = nn.Conv2d(in_channels = in_channels, out_channels = 64, kernel_size = 1, stride = 1 , padding = 'same')
-        self.branch1 = nn.Sequential(
-            nn.Conv2d(in_channels = in_channels, out_channels = 64, kernel_size = 1, stride = 1, padding = 'same'),
-            nn.Conv2d(in_channels = 64, out_channels = 64, kernel_size = (1,3), stride = 1, padding = 'same'),
-            nn.Conv2d(in_channels = 64, out_channels = 64, kernel_size = (3,1), stride = 1, padding = 'same')
-        )
-        self.conv_up = nn.Conv2d(in_channels = 128, out_channels = 448, kernel_size = 1, stride = 1, padding = 'same')
-
-
-    def forward(self, x):
-        branch0 = self.branch0(x)
-        branch1 = self.branch1(x)
-        mixed = torch.cat([branch0, branch1], dim = 1)
-        up = self.conv_up(mixed)
-        return F.relu(x + self.scale * up)
-
-class ReductionB(nn.Module):
-    def __init__(self):
-        super(ReductionB, self).__init__()
-        self.branch0 = nn.Sequential(
-            nn.Conv2d(in_channels = 448, out_channels = 128, kernel_size = 1, stride = 1, padding = 'same'),
-            nn.Conv2d(in_channels = 128, out_channels = 192, kernel_size = 3, stride = 1, padding = 'valid')
-        )
-        self.branch1 = nn.Sequential(
-            nn.Conv2d(in_channels = 448, out_channels = 128, kernel_size = 1, stride = 1, padding = 'same'),
-            nn.Conv2d(in_channels = 128, out_channels = 128, kernel_size = 3, stride = 1, padding = 'valid')
-        )
-        self.branch2 = nn.Sequential(
-            nn.Conv2d(in_channels = 448, out_channels = 128, kernel_size = 1, stride = 1, padding = 'same'),
-            nn.Conv2d(in_channels = 128, out_channels = 128, kernel_size = 3, stride = 1, padding = 'same'),
-            nn.Conv2d(in_channels = 128, out_channels = 128, kernel_size = 3, stride = 1, padding = 'valid')
-        )
-
-        self.branch3 = nn.MaxPool2d(kernel_size = 3, stride = 1, padding = 0)
-
-    def forward(self, x):
-        branch0 = self.branch0(x)
-        branch1 = self.branch1(x)
-        branch2 = self.branch2(x)
-        branch3 = self.branch3(x)
-        mixed = torch.cat([branch0, branch1, branch2, branch3], dim = 1)
-        return mixed
-
-
-# Inception-ResNet Model
-class InceptionResNetV1(nn.Module):
-    def __init__(self, num_classes=2):
-        super(InceptionResNetV1, self).__init__()
-        self.stem = InceptionStem()
-        self.a_block = InceptionResNetABlock()
-        self.b_block = InceptionResNetBBlock()
-        self.red_a = ReductionA()
-        self.red_b = ReductionB()
-        self.global_pool = nn.AdaptiveAvgPool2d((1,1))
-        self.dropout = nn.Dropout(0.2)
-        self.fc = nn.Linear(896, num_classes)
-        
-
-    def forward(self, x):
-        x = self.stem(x)
-        x = self.a_block(x)
-        x = self.red_a(x)
-        x = self.b_block(x)        
-        x = self.red_b(x)
-        x = self.global_pool(x)
-        x = torch.flatten(x, 1)
-        x = self.dropout(x)
-        x = self.fc(x)
-        return F.log_softmax(x, dim = 1)
-
 
 def load_labels(label_file):
     """Load image labels from the label file."""
@@ -188,50 +54,12 @@ def load_dataset(data_dir, label_file, is_train):
     return data_loader
 
 
-
-def train_wisa(model, device, train_loader, optimizer, criterion, epoch):
-    model.train()
-    correct = 0
-    total = 0
-    running_loss = 0
-
-    for batch_idx, (data, target) in enumerate(train_loader):
-        data, target = data.to(device), target.to(device)
-        optimizer.zero_grad()
-        output = model(data)
-        loss = criterion(output, target)
-        loss.backward()
-        optimizer.step()
-        
-        # Track running loss
-        running_loss += loss.item()
-        
-        # Calculate accuracy
-        pred = output.argmax(dim=1, keepdim=False)  # Get the predicted class
-        correct += pred.eq(target).sum().item()
-        total += target.size(0)
-
-        #if batch_idx % 10_000 == 0:  # Adjust this to suit your dataset size
-        accuracy = 100. * correct / total
-        print(f"Train Epoch: {epoch} [{batch_idx * len(data)}/{len(train_loader.dataset)}] "
-                  f"Loss: {loss.item():.6f} Accuracy: {accuracy:.2f}%")
-    
-    print("Training complete!")
-    torch.save(model.state_dict(), './Trained_Models/wisa.pth')
-
-    # Print overall training loss and accuracy for the epoch
-    overall_accuracy = 100. * correct / len(train_loader.dataset)
-    print(f"Epoch {epoch} Summary: Average Loss: {running_loss / len(train_loader):.6f}, "
-          f"Accuracy: {overall_accuracy:.2f}%")
-    
 def train_model(train_loader, device,model_type):
     
     """Train the model on the training dataset."""
     if model_type == 'densenet161':
         model = models.densenet161(weights=models.DenseNet161_Weights.DEFAULT)
         model.classifier = nn.Linear(model.classifier.in_features, 2)
-    elif model_type == 'wisa':
-        model = InceptionResNetV1(num_classes=2)
 
    
     criterion = nn.CrossEntropyLoss()
@@ -272,43 +100,6 @@ def train_model(train_loader, device,model_type):
     torch.save(model.state_dict(), './Trained_Models/new_d161.pth')
 
     return model
-
-def test_wisa(model, device, test_loader, criterion):
-
-    model.load_state_dict(torch.load('./Trained_Models/only_dos_d161.pth', weights_only='True'))
-    model.eval()
-    test_loss = 0
-    correct = 0
-    all_preds = []
-    all_targets = []
-
-    with torch.no_grad():
-        for data, target in test_loader:
-            data, target = data.to(device), target.to(device)
-            output = model(data)
-            test_loss += criterion(output, target).item()
-            pred = output.argmax(dim=1, keepdim=False)
-            correct += pred.eq(target).sum().item()
-            all_preds.extend(pred.cpu().numpy())
-            all_targets.extend(target.cpu().numpy())
-    
-    test_loss /= len(test_loader.dataset)
-    accuracy = 100. * correct / len(test_loader.dataset)
-    precision = precision_score(all_targets, all_preds, average='weighted')
-    recall = recall_score(all_targets, all_preds, average='weighted')
-    f1 = f1_score(all_targets, all_preds, average='weighted')
-    
-    print(f"\nTest set: Average loss: {test_loss:.4f}, Accuracy: {correct}/{len(test_loader.dataset)} ({accuracy:.2f}%)")
-    print(f"Precision: {precision:.8f}, Recall: {recall:.8f}, F1-score: {f1:.8f}\n")
-    
-    # Confusion matrix
-    cm = confusion_matrix(all_targets, all_preds)
-    plt.figure(figsize=(8, 6))
-    # sns.heatmap(cm, annot=True, fmt='d', cmap='Blues', xticklabels=np.unique(all_targets), yticklabels=np.unique(all_targets))
-    plt.title('Confusion Matrix')
-    plt.ylabel('True Labels')
-    plt.xlabel('Predicted Labels')
-    plt.show()
 
 def test_model(test_loader, device,model):
 

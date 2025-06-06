25f668c39ca4ea1dea07249951aac81e84af2e5a
Isha Pali <ishapali30.1997@gmail.com>
2025-06-06 17:18:28 +0530

    Create Target_IDS_D201.py

---
+++ PLAID/Target_IDS_D201.py
@@ -0,0 +1,205 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import DataLoader, TensorDataset
+from torchvision import datasets, transforms, models
+import numpy as np
+import os
+# import seaborn as sns
+from PIL import Image
+import matplotlib.pyplot as plt
+import torch.nn.functional as F
+from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
+from sklearn.metrics import precision_score, recall_score, f1_score, confusion_matrix, accuracy_score
+
+
+
+# Define transformations and dataset paths
+data_transforms = {
+    'test': transforms.Compose([transforms.ToTensor()]),
+    'train': transforms.Compose([transforms.ToTensor()])
+}
+
+
+def load_labels(label_file):
+    """Load image labels from the label file."""
+    labels = {}
+    with open(label_file, 'r') as file:
+        for line in file:
+            filename, label = line.strip().replace("'", "").replace('"', '').split(': ')
+            labels[filename.strip()] = int(label.strip())
+    return labels
+
+def load_dataset(data_dir, label_file, is_train):
+    """Load datasets and create DataLoader."""
+    image_labels = load_labels(label_file)
+    images = []
+    labels = []
+
+    for filename, label in image_labels.items():
+        img_path = os.path.join(data_dir, filename)
+        if os.path.exists(img_path):
+            image = Image.open(img_path).convert("RGB")
+            image = data_transforms['train' if is_train else 'test'](image)
+            images.append(image)
+            labels.append(label)
+
+    images_tensor = torch.stack(images)
+    labels_tensor = torch.tensor(labels)
+    dataset = TensorDataset(images_tensor, labels_tensor)
+    batch_size = 32 if is_train else 1
+    data_loader = DataLoader(dataset, batch_size=batch_size, shuffle=is_train, num_workers=4)
+
+    print(f'Loaded {len(images)} images.')
+    return data_loader
+
+    
+def train_model(train_loader, device,model_type,save_model):
+    
+    """Train the model on the training dataset."""
+    if model_type == 'densenet201':
+        model = models.densenet201(weights=models.DenseNet201_Weights.DEFAULT)
+        model.classifier = nn.Linear(model.classifier.in_features, 2)
+
+   
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
+
+    model = model.to(device)
+
+    num_epochs = 10
+    for epoch in range(num_epochs):
+        print(f'Epoch {epoch}/{num_epochs - 1}')
+        print('-' * 10)
+
+        model.train()
+        running_loss = 0.0
+        running_corrects = 0
+
+        for inputs, labels in train_loader:
+            inputs = inputs.to(device)
+            labels = labels.to(device)
+
+            optimizer.zero_grad()
+            outputs = model(inputs)
+            _, preds = torch.max(outputs, 1)
+            loss = criterion(outputs, labels)
+
+            loss.backward()
+            optimizer.step()
+
+            running_loss += loss.item() * inputs.size(0)
+            running_corrects += torch.sum(preds == labels.data)
+
+        epoch_loss = running_loss / len(train_loader.dataset)
+        epoch_acc = running_corrects.double() / len(train_loader.dataset)
+
+        print(f'Train Loss: {epoch_loss:.4f} Acc: {epoch_acc:.4f}')
+
+    print("Training complete!")
+    torch.save(model.state_dict(), f'./Trained_Models/{save_model}.pth')
+
+    return model
+
+
+def test_model(test_loader, device,model,save_model):
+
+    model.load_state_dict(torch.load(f'./Trained_Models/{save_model}.pth', weights_only='True'))
+    model.eval()
+    # Initialize lists to store predictions and labels
+    all_preds = []
+    all_labels = []
+
+    # Evaluate the model on the test dataset
+    for inputs, labels in test_loader:
+        inputs, labels = inputs.to(device), labels.to(device)  # Move data to the appropriate device
+        with torch.no_grad():  # Disable gradient calculation for evaluation
+            outputs = model(inputs)  # Forward pass
+            _, preds = torch.max(outputs, 1)  # Get predicted class labels
+        
+        # Store predictions and labels
+        all_preds.extend(preds.cpu().numpy())
+        all_labels.extend(labels.cpu().numpy())
+
+    # Convert lists to numpy arrays
+    all_preds = np.array(all_preds)
+    all_labels = np.array(all_labels)
+
+    # Calculate accuracy
+    test_accuracy = np.sum(all_preds == all_labels) / len(all_labels)
+    print(f'Test Accuracy: {test_accuracy:.4f}')
+
+    return all_preds, all_labels  # Return accuracy for potential further use
+
+def evaluation_metrics(all_preds, all_labels):
+
+    # Generate confusion matrix
+    cm = confusion_matrix(all_labels, all_preds)
+
+    # Display confusion matrix
+    disp = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=[0, 1])
+    disp.plot(cmap=plt.cm.Blues)
+    plt.title('Confusion Matrix')
+    # plt.savefig('./CF_Images_Inject20_modifygrad_d161', dpi=300)
+    plt.savefig('./CF_Results/Target_IDS_d201.png', dpi=300)
+    plt.show()
+    
+
+    # Now you can access the true negatives and other metrics
+    true_negatives = cm[0, 0]
+    false_positives = cm[0, 1]
+    false_negatives = cm[1, 0]
+    true_positives = cm[1, 1]
+
+    IDS_accu = accuracy_score(all_labels, all_preds) 
+    IDS_prec = precision_score(all_labels, all_preds)
+    IDS_recall = recall_score (all_labels,all_preds)
+    IDS_F1 = f1_score(all_labels,all_preds)
+    
+    return IDS_accu, IDS_prec, IDS_recall, IDS_F1
+
+
+def main():
+
+    
+    # Define your dataset directories
+    train_dataset_dirs = 'Surrogate_dataset_old/combined_folder_DoS_spoof'  # Add the paths to your other training directories
+    train_label_files = 'Surrogate_dataset_old/combined_folder_DoS_spoof/combined_labels_dos_spoof.txt'
+    
+    test_dataset_dirs = 'Surrogate_dataset_old/combined_folder_DoS_spoof_test' # Same for test directories
+    test_label_files = 'Surrogate_dataset_old/combined_folder_DoS_spoof_test/combined_labels_dos_spoof_test.txt'
+    # Define the corresponding label files for each directory
+    
+    
+
+    # Set up the device (GPU or CPU)
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+    # Load the test and train datasets from multiple folders
+    train_loader = load_dataset(train_dataset_dirs, train_label_files,is_train=True)
+    print("Loaded train dataset")
+
+    test_loader = load_dataset(test_dataset_dirs, test_label_files,is_train=False)
+    print("Loaded test dataset")
+    
+    model_type = 'densenet201'  # Change to the desired model type
+    model = models.densenet201(weights=models.DenseNet201_Weights.DEFAULT)
+    model.classifier = nn.Linear(model.classifier.in_features, 2)
+    model = model.to(device)
+
+    save_model = 'IDS_D201'
+    # Train the model
+    model = train_model(train_loader, device,model_type,save_model)
+
+    all_preds, all_labels = test_model(test_loader, device, model,save_model )
+
+    IDS_accu, IDS_prec, IDS_recall,IDS_F1 = evaluation_metrics(all_preds, all_labels)
+    print("----------------IDS Perormance Metric----------------")
+    print(f'Accuracy: {IDS_accu:.4f}')
+    print(f'Precision: {IDS_prec:.4f}')
+    print(f'Recall: {IDS_recall:.4f}')
+    print(f'F1 Score: {IDS_F1:.4f}')
+    
+
+if __name__ == "__main__":
+    main()

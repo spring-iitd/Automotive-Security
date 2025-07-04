8e1828e87c3013f65c786e426d3a9381c0b3ff0a
Ashish Modi <ashishmodi30@gmail.com>
2025-07-04 12:44:21 +0530

    Replaced inline feature extraction with reusable feature extractor module

--- CAN IDS Pipeline/ids/DecisionTree.py
+++ CAN IDS Pipeline/ids/DecisionTree.py
@@ -2,37 +2,45 @@ from ids.base import IDS
 from sklearn.metrics import accuracy_score
 from sklearn.tree import DecisionTreeClassifier
 import joblib
-from sklearn.preprocessing import StandardScaler
+from features import feature_extractor
+from config import *
 
 class DecisionTree(IDS):
     def __init__(self):
         self.dt = DecisionTreeClassifier(max_depth = 4)
 
-    def train(self, X_train, Y_train, **kwargs):
-        super().train(X_train, Y_train)
+    def train(self, **kwargs):
+        super().train()
         self.dt.fit(self.X, self.Y)
 
-    def test(self, X_test, Y_test):
-        super().train(X_test, Y_test)
+    def test(self, **kwargs):
+        super().test()
         Y_pred = self.predict(self.X)
         return accuracy_score(self.Y, Y_pred)
 
     def save(self, path):
         joblib.dump(self.dt, path)
-
+    
     def predict(self, X_test):
-        super().predict(X_test)
-        dt_preds = self.dt.predict(self.X)
+        # super().predict(X_test)
+        dt_preds = self.dt.predict(X_test)
         return dt_preds
 
     def load(self, path):
         self.dt = joblib.load(path)
 
-    def preprocess(self, X, Y):
-        scaler = StandardScaler()
-        scaler.fit(X)
+    def extract_features(self):
+        return feature_extractor.extract_features()
 
-        # Transform train and test sets
-        X = scaler.transform(X)
+        # dataset_path = os.path.join(DIR_PATH, "..", "datasets", DATASET_NAME)
 
-        return X, Y
+        # dataframe = stat_features.extract_features(dataset_path)
+        # X, Y = dataframe.drop(columns = ['flag', 'timestamp']).values, dataframe['flag'].values
+
+        # scaler = StandardScaler()
+        # scaler.fit(X)
+
+        # # Transform train and test sets
+        # X = scaler.transform(X)
+
+        # return X, Y
--- CAN IDS Pipeline/ids/RandomForest.py
+++ CAN IDS Pipeline/ids/RandomForest.py
@@ -2,18 +2,19 @@ from ids.base import IDS
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
 import joblib
-from sklearn.preprocessing import StandardScaler
+from config import *
+from features import feature_extractor
 
 class RandomForest(IDS):
     def __init__(self):
         self.rf = RandomForestClassifier(n_estimators=100, max_depth=4)
 
-    def train(self, X_train, Y_train, **kwargs):
-        super().train(X_train, Y_train)
+    def train(self, **kwargs):
+        super().train()
         self.rf.fit(self.X, self.Y)
 
-    def test(self, X_test, Y_test):
-        super().test(X_test, Y_test)
+    def test(self):
+        super().test()
         Y_pred = self.predict(self.X)
         return accuracy_score(self.Y, Y_pred)
 
@@ -21,18 +22,26 @@ class RandomForest(IDS):
         joblib.dump(self.rf, path)
 
     def predict(self, X_test):
-        super().predict(X_test)
-        dt_preds = self.rf.predict(self.X)
-        return dt_preds
+        # super().predict(X_test)
+        rf_preds = self.rf.predict(X_test)
+        return rf_preds
 
     def load(self, path):
         self.rf = joblib.load(path)
 
-    def preprocess(self, X, Y):
-        scaler = StandardScaler()
-        scaler.fit(X)
+    def extract_features(self):
+        return feature_extractor.extract_features()
 
-        # Transform train and test sets
-        X = scaler.transform(X)
+        # dataset_path = os.path.join(DIR_PATH, "..", "datasets", DATASET_NAME)
 
-        return X, Y
+        # dataframe = stat_features.extract_features(dataset_path)
+        # X, Y = dataframe.drop(columns = ['flag', 'timestamp']).values, dataframe['flag'].values
+
+        
+        # scaler = StandardScaler()
+        # scaler.fit(X)
+
+        # # Transform train and test sets
+        # X = scaler.transform(X)
+
+        # return X, Y
